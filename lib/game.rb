bf95c2a125bfb5b46cf59939742661ccc18afd65
joemecha <jwmecha@gmail.com>
2021-02-08 19:04:06 -0600

    Add sudo code to game file

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+# game class
+
+# display game message
+# generates random secret code
+
+#play enters guess
+  # if q, exit
+  # if c, display secret code
+  # if matches secret code, go to end_game
+
+  # if too short/long, display message, get user guess again
+
+  # if valid guess, compare to secret code and give feedback
+  # insert guess_counter
+
+  # increment guess_counter
+  # repeat loop
