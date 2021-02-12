c863a4bf953b11687682ebd9d36d151ce14e0679
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-11 21:08:39 -0500

    Add instance variable and methods_keypegs

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,69 +1,46 @@
-class Turn
-  attr_accessor :guess, :code_guess, :secret_code, :all_possible_codes, :red_peg, :white_peg
+require './lib/game'
 
-  def initialize(guess)
+
+class Turn
+  attr_accessor :guess,
+                :code_guess,
+                :secret_code,
+                :all_possible_codes,
+                :red_peg,
+                :white_peg
+
+  def initialize(guess, secret_code)
     @guess = guess
-    @code_guess = []
-    @secret_code = []
+    @code_guess = @guess.code_guess
+    @secret_code = secret_code
     @red_peg = 0
     @white_peg = 0
-    @all_possible_codes = ["r", "g", "b", "y"].repeated_permutation(4).to_a
+
   end
 
   def find_key_colors
+    # Red pegs computed below 23-26
     @red_peg = 0
-    @white_peg = 0
     @code_guess.each_with_index do |color, index|
-      if color == @secret_code[index]
-        @red_peg += 1
-      else
+     if color == @secret_code[index]
+       @red_peg += 1
+     end
+    end
+    # White pegs are computed below
+    color_checker = []
+    @code_guess.each do |letter|
+      if @secret_code.include?(letter) && color_checker.include?(letter) == false
         @white_peg += 1
+        color_checker << letter
       end
     end
   end
 
-  def make_secret_code
-    @secret_code = all_possible_codes.sample
-  end
-
-  def get_code
-    if @guess == 'q' || @guess == "quit"
-      p "You have exited the game"
-    elsif @guess == 'c' || @guess == "cheat"
-      p "Cheat code: '#{@secret_code.join}'"
-    else
-      a = @guess.chars
-      a.each do |letter|
-        @code_guess << letter
-      end
-    end
+  def get_secret_code
+    p @secret_code
   end
 
   def guessed_secret_code?
     @code_guess == @secret_code
   end
-
-  def too_short?
-    if @code_guess.length <= 3
-      p "Your guess is too short"
-    end
-  end
-
-  def too_long?
-    if @code_guess.length >= 5
-      p "Your guess is too long"
-    end
-  end
-  #turn would have player
-  #turn would submit one guess
-  #turn would provide feedback
-  #turn would indicate a win
-  # increment guess_counter
-  # repeat loop
 end
-
-
-# array method to create all possibilities of game
-# all_possible_codes = ["r", "g", "b", "y"].repeated_permutation(4).to_a
-# p all_possible_codes.sample
-# >>>>>>> c5ecc780e51f1ae8604011dac8aa6a5ba1a20fbc
