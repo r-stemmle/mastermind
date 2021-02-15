93cbf123a548bf7337861169bf0bc7e0327ad421
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-15 17:17:47 -0500

    Add top10 feature to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 require './lib/turn'
 require './lib/guess'
-
+require 'json'
 
 class Game
   attr_reader :input,
@@ -103,6 +103,87 @@ class Game
     end
   end
 
+  def winner
+    total_seconds_elapsed = (Time.now - @start_time).round(0)
+    @game_minutes = ((total_seconds_elapsed % 3600) / 60).to_i
+    @game_seconds = total_seconds_elapsed - (@game_minutes * 60)
+    p "Congratulations! You've guessed the sequence! What's your name?"
+    puts ""
+    print ">"
+    name = gets.chomp
+    puts ""
+    player = Player.new(name, @secret_code.join().upcase, @guess_count, (@game_minutes*60) + @game_seconds)
+    my_object = {name: player.name, sequence: player.sequence, guess_count: player.guess_count, timer: player.play_time}
+
+    #array with all historical number of guesses
+    all_guess_count = []
+    all_seconds_count = []
+    #iteration to collect latest guess counts
+    IO.foreach("winners.json") do |winner|
+      each_hash = JSON.parse(winner)
+      all_guess_count << each_hash["guess_count"]
+      all_seconds_count << each_hash["timer"]
+    end
+    guess_array_sum = all_guess_count.inject(0) { |sum, x| sum + x }
+    @average_guesses = guess_array_sum/all_guess_count.length
+
+    seconds_array_sum = all_seconds_count.inject(0) { |sum, x| sum + x }
+    @average_seconds = seconds_array_sum/all_seconds_count.length
+    # adds each players data to winners.json
+    File.open("winners.json", "a+") do |winner_info|
+      winner_info.puts(my_object.to_json)
+    end
+    puts "#{player.name}, you guessed the sequence '#{@secret_code.join().upcase}' in #{@guess_count} guesses over #{@game_minutes} minutes, #{@game_seconds} seconds. That's #{time_comparer} and #{guess_comparer} than the average."
+    puts ""
+    puts "=== TOP 10 ==="
+    ranking_array = []
+    IO.foreach("winners.json") do |winner|
+      each_h = JSON.parse(winner)
+      ranking_array << each_h
+    end
+    b = ranking_array.sort_by do |a|
+      a["guess_count"]
+    end
+
+    puts "1. #{b[0]["name"]} solved '#{b[0]["sequence"]}' in #{b[0]["guess_count"]} guesses over #{b[0]["timer"].divmod(60)[0]}m#{b[0]["timer"].divmod(60)[1]}s."
+    puts "2. #{b[1]["name"]} solved '#{b[1]["sequence"]}' in #{b[1]["guess_count"]} guesses over #{b[1]["timer"].divmod(60)[0]}m#{b[1]["timer"].divmod(60)[1]}s."
+    puts "3. #{b[2]["name"]} solved '#{b[2]["sequence"]}' in #{b[2]["guess_count"]} guesses over #{b[2]["timer"].divmod(60)[0]}m#{b[2]["timer"].divmod(60)[1]}s."
+    puts "4. #{b[3]["name"]} solved '#{b[3]["sequence"]}' in #{b[3]["guess_count"]} guesses over #{b[3]["timer"].divmod(60)[0]}m#{b[3]["timer"].divmod(60)[1]}s."
+    puts "5. #{b[4]["name"]} solved '#{b[4]["sequence"]}' in #{b[4]["guess_count"]} guesses over #{b[4]["timer"].divmod(60)[0]}m#{b[4]["timer"].divmod(60)[1]}s."
+    puts "6. #{b[5]["name"]} solved '#{b[5]["sequence"]}' in #{b[5]["guess_count"]} guesses over #{b[5]["timer"].divmod(60)[0]}m#{b[5]["timer"].divmod(60)[1]}s."
+    puts "7. #{b[6]["name"]} solved '#{b[6]["sequence"]}' in #{b[6]["guess_count"]} guesses over #{b[6]["timer"].divmod(60)[0]}m#{b[6]["timer"].divmod(60)[1]}s."
+    puts "8. #{b[7]["name"]} solved '#{b[7]["sequence"]}' in #{b[7]["guess_count"]} guesses over #{b[7]["timer"].divmod(60)[0]}m#{b[7]["timer"].divmod(60)[1]}s."
+    puts "9. #{b[8]["name"]} solved '#{b[8]["sequence"]}' in #{b[8]["guess_count"]} guesses over #{b[8]["timer"].divmod(60)[0]}m#{b[8]["timer"].divmod(60)[1]}s."
+    puts "10. #{b[9]["name"]} solved '#{b[9]["sequence"]}' in #{b[9]["guess_count"]} guesses over #{b[9]["timer"].divmod(60)[0]}m#{b[9]["timer"].divmod(60)[1]}s."
+
+  end
+
+  def minute_finder(seconds)
+    [1, 19]
+  end
+
+  def time_comparer
+    if ((@game_minutes*60) + @game_seconds) > @average_seconds
+      bummer = (((@game_minutes*60) + @game_seconds) - @average_seconds).divmod(60)
+      "#{bummer[0]} minutes, #{bummer[1]} seconds slower"
+    elsif ((@game_minutes*60) + @game_seconds) < @average_seconds
+      bummer = (@average_seconds - ((@game_minutes*60) + @game_seconds)).divmod(60)
+      "#{bummer[0]} minutes, #{bummer[1]} seconds faster"
+    else
+      "same amount of time"
+    end
+  end
+
+  def guess_comparer
+    if @guess_count > @average_guesses
+      "#{@guess_count - @average_guesses} guesses more"
+    elsif @guess_count < @average_guesses
+      "#{@average_guesses - @guess_count} guesses fewer"
+    else
+      "no different"
+    end
+  end
+
   def play_message
     p "You have chosen the #{difficulty_level}. Mastermind has constructed a secret sequence with #{difficulty_level} elements made up of: (r)ed, (g)reen, (b)lue, (y)ellow#{intermediate_color} #{advanced_color} Use (q)uit at any time to end the game.  What's your guess?"
     print ">"
@@ -112,24 +193,6 @@ class Game
     p "You have 4, 6, or 8 (depending on selected difficulty level) color coded pegs to play in any combination for one given guess. Mastermind will provide feedback on the number of correct colors, and the number of correct positions."
     puts "===" * 20
     welcome
-  end
-
-  def winner
-    total_seconds_elapsed = (Time.now - @start_time).round(0)
-    game_minutes = ((total_seconds_elapsed % 3600) / 60).to_i
-    game_seconds = total_seconds_elapsed - (game_minutes * 60)
-    p "Congratulations! You've guessed the sequence! What's your name?"
-    puts ""
-    print ">"
-    name = gets.chomp
-    puts ""
-    player = Player.new(name, @secret_code.join().upcase, @guess_count, [game_minutes, game_seconds])
-    puts "#{player.name}, you guessed the sequence '#{@secret_code.join().upcase}' in #{@guess_count} guesses over #{game_minutes} minutes, #{game_seconds} seconds. That's 1 minute, 10 seconds faster and two guesses fewer than the average."
-    puts ""
-    puts "=== TOP 10 ==="
-    puts "1. #{player.name} solved #{player.sequence} in #{player.guess_count} guesses over #{game_minutes}m#{game_seconds}s"
-    # p "Congratulations! You guessed the sequence '#{@secret_code.join().upcase}' in #{@guess_count} guesses over #{game_minutes} minutes, #{game_seconds} seconds."
-    # require "pry"; binding.pry
   end
 
   def feedback
